d505698d058f99373aa706df8caef57b8ce343bb
Selena Sean <selena.sean.1994@gmail.com>
2023-04-06 17:03:02 +0200

    commit 1

---
+++ app/src/main/java/fr/selquicode/mareu/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package fr.selquicode.mareu.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fr.selquicode.mareu.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
